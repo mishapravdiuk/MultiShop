53412a46cf1509924d7aed44e8e5d70e4b6c54cc
Misha <mishapravdiuk@gmail.com>
2022-09-29 10:28:01 +0300

    Modify models

--- MultiShop/shop/models.py
+++ MultiShop/shop/models.py
@@ -1,5 +1,7 @@
 from ctypes import sizeof
+from distutils.command.upload import upload
 from platform import release
+from tabnanny import verbose
 from turtle import color
 from unicodedata import category, name
 from django.db import models
@@ -34,15 +36,16 @@ class ProductInfo(models.Model):
     title=models.CharField(max_length=255)
     size=models.ManyToManyField(Size, related_name="product")
     color=models.CharField(max_length=255)
-    category=models.ForeignKey(Category, on_delete=models.CASCADE, related_name="product")
+    category=models.ForeignKey(Category, on_delete=models.PROTECT, related_name="product")
     price=models.FloatField()
     description=models.TextField()
-    photo=models.ImageField()
+    photo=models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     # reviews 
     # rating 
     quantity=models.IntegerField()
     created_at=models.DateTimeField(auto_now_add=True)
     slug=models.SlugField(max_length=255, unique=True) 
+    views = models.IntegerField(default=0, verbose_name='Number of views')
 
     def __str__(self):
         return self.title
